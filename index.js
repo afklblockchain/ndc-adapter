d3c190d67578e7a90cd0ea6f017d93a729236ec2
alicedevries <alice-de.vries@klmc.om>
2018-10-25 12:16:08 +0200

    use relative path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var request = require('request-promise');
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
+const path = require("path");
 
 
 var NDC_ADAPTER = function(url, apiKey) {
@@ -11,7 +12,7 @@ var NDC_ADAPTER = function(url, apiKey) {
 
 
     adapter.AirShopping = function (origin, destination, date) {
-        var body = fs.readFileSync("./airshopping.xml").toString()
+        var body = fs.readFileSync(path.resolve(__dirname, "airshopping.xml")).toString()
             .replace('{{destination}}', destination)
             .replace('{{origin}}', origin)
             .replace('{{date}}', date)
@@ -29,7 +30,6 @@ var NDC_ADAPTER = function(url, apiKey) {
                 console.log("ERROR")
                 return;
             }
-            // console.log(body)
             return body;
         })
     }
@@ -37,7 +37,5 @@ var NDC_ADAPTER = function(url, apiKey) {
     return adapter;
 
 }
-var NPM = NDC_ADAPTER('https://ndc-rct.airfranceklm.com/passenger/distribmgmt/001448v01/EXT?', '3nnfesjhfupgh9dbb42yay55')
-NPM.AirShopping('AMS', 'JFK', '2018-11-02').then(function(body) {console.log('done', body)})
 
 module.exports = NDC_ADAPTER;
